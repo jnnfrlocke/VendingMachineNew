7ab0e5bb981ec1ac9fbe25113efb686d0c74e47b
Jen <jnnfrlocke@gmail.com>
2017-10-24 15:52:08 -0500

    update db, start Machines.cs

---
+++ Models/Machines.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+
+namespace VendingMachineNew.Models
+{
+    public class Machines
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+
+    }
+}
