4c87b79b47f24b1998157830b32869735ad657d4
Matthew Miranda <matthewmiranda32@gmail.com>
2017-10-25 10:07:58 -0500

    MachineModelandControllerAdd

--- Models/Inventory.cs
+++ Models/Inventory.cs
@@ -9,10 +9,15 @@ namespace VendingMachine.Models
     public class Inventory
     {
            [Key]
+            [Display(Name = "Product ID")]
             public int id { get; set; }
+            [Display(Name = "Product Type")]
             public string typeProduct { get; set; }
+            [Display(Name = "Product Name")]
             public string name { get; set; }
+            [Display(Name = "Quantity")]
             public int quantity { get; set; }
+            [Display(Name = "Price")]
             public double price { get; set; }
 
     }
