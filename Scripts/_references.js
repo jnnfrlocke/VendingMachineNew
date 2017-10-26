d4269de0388db777cd90ed92558d4b097aa245a6
jnnfrlocke <jnnfrlocke@gmail.com>
2017-10-26 11:16:25 -0500

    Update References.js for merge

--- Scripts/_references.js
+++ Scripts/_references.js
@@ -7,3 +7,4 @@
 /// <reference path="jquery.validate.unobtrusive.min.js" />
 /// <reference path="jquery-1.10.2.min.js" />
 /// <reference path="respond.min.js" />
+/// <reference path="modernizr-2.6.2.js" />
